14a0b7c1aa436817cd7e819d376e4cfef3b41084
Bhag chandra <bkrajpoot08021997@gmail.com>
2023-09-11 12:10:01 +0530

    Handle

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+
+        try {
+            int c = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("You should revise mathematics");
+        }
+    }
+}
